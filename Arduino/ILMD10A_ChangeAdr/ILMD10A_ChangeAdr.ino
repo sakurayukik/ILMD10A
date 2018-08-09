f4d0d7c8764f0cdeb5b5cd19edee5f108417e1a6
sakurayukik <42203125+sakurayukik@users.noreply.github.com>
2018-08-10 01:01:09 +0900

    Change slave address sample

---
+++ Arduino/ILMD10A_ChangeAdr/ILMD10A_ChangeAdr.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("init....");
+  Wire.begin();
+  delay(1000);
+}
+
+uint8_t adr = 0x10; //Current slave address
+uint8_t adrNew = 0x11; //Current slave address
+
+void loop() {
+  Wire.beginTransmission(adr);
+  Wire.write(0x09);
+  Wire.write(adrNew);
+  Wire.write(0xFF);
+  Wire.endTransmission();
+  delay(500);
+  
+  Wire.beginTransmission(adr);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  delay(100);
+  
+  Wire.requestFrom(adr, 9);
+  while (Wire.available()) {
+    uint8_t c = Wire.read();
+    Serial.print("0x");
+    Serial.print(c,HEX);
+    Serial.print("\n");
+  }
+  Serial.print("New address is 0x");
+  Serial.println(adrNew,HEX);
+  while(1){
+  }
+}
+
+
+
